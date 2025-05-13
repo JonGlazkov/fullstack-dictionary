db41eacba4f53e8ae63600c3588044e230b96856
Jon Ventura <jonathan_vmaced@hotmail.com>
2025-05-13 12:08:57 -0300

    [CHORE]: Added axios

--- web/src/app/auth/sign-up/page.tsx
+++ web/src/app/auth/sign-up/page.tsx
@@ -5,7 +5,6 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { api } from "@/lib/axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";

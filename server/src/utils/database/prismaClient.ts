c237b4098b0852f99b094a8008cc1585e817083a
Jon Ventura <jonathan_vmaced@hotmail.com>
2025-05-15 10:40:41 -0300

    [CHORE]: adjusted prisma

--- server/src/utils/database/prismaClient.ts
+++ server/src/utils/database/prismaClient.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "prisma/generated/client";
+import { PrismaClient } from "@prisma/client";
 
-const client = globalThis.prisma || new PrismaClient();
-
-export { client as prisma };
+const prisma = globalThis.prisma || new PrismaClient();
+export { prisma };

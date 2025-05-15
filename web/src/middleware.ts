3f2aea2be6b5d3611b4da0f850cdafbb9f9c4b94
Jon Ventura <jonathan_vmaced@hotmail.com>
2025-05-15 09:21:43 -0300

    [CHORE]: Adjusted middleware removing start page

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,20 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getUrl } from './lib/get-url'
+import { NextRequest, NextResponse } from "next/server";
+import { getUrl } from "./lib/get-url";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('authjs.session-token')
-  const pathname = request.nextUrl.pathname
-  const baseUrl = request.nextUrl.origin
+  const token = request.cookies.get("authjs.session-token");
+  const pathname = request.nextUrl.pathname;
+  const baseUrl = request.nextUrl.origin;
 
-  if (!pathname.includes('/app') && token) {
-    return NextResponse.redirect(new URL(getUrl('/app'), baseUrl))
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL(getUrl("/app"), baseUrl));
   }
 
-  if (pathname.includes('/app') && !token) {
-    return NextResponse.redirect(new URL(getUrl('/auth/sign-in'), baseUrl))
+  if (!pathname.includes("/app") && token) {
+    return NextResponse.redirect(new URL(getUrl("/app"), baseUrl));
+  }
+
+  if (pathname.includes("/app") && !token) {
+    return NextResponse.redirect(new URL(getUrl("/auth/sign-in"), baseUrl));
   }
 }
 
 export const config = {
-  matcher: ['/app/:path*', '/auth'],
-}
+  matcher: ["/", "/app/:path*", "/auth"],
+};
